088f1091c808c85ec60552575d498f8a33a1108d
drbortel <drbortel@gmail.com>
2015-09-15 14:49:26 -0500

    input form to servlet update

--- src/com/usaa/webopoly/WebOpolyServlet.java
+++ src/com/usaa/webopoly/WebOpolyServlet.java
@@ -1,5 +1,6 @@
 package com.usaa.webopoly;
 
+import java.io.*;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -24,9 +25,24 @@ public class WebOpolyServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	// TODO Auto-generated method stub
+    	response.getWriter().append("Served at: ").append(request.getContextPath());
 
+    	response.setContentType("text/html");
+    	PrintWriter out = response.getWriter();
+
+    	String numPlayers = request.getParameter("numPlayers");
+    	String startMoney = request.getParameter("startMoney");
+    	
+    	out.println("<HTML>");
+    	out.println("<HEAD><TITLE>Start Info for Game</TITLE></HEAD>");
+    	out.println("<BODY>");
+       	out.println("You will have " + numPlayers + " players!</TITLE></HEAD>");
+    	out.println("You will have " + startMoney + " starting money!</TITLE></HEAD>");
+    	out.println("</BODY></HTML>");
+
+    }
+	
+	
 }
